71cc010561b9838c922dcb4290f164a5cbc7e534
SToan11 <143256909+SToan11@users.noreply.github.com>
2025-11-25 01:54:23 +0700

    BookHub

---
+++ BookHub/Program.cs
@@ -0,0 +1,49 @@
+﻿using BookHub.Client.Pages;
+using BookHub.Components;
+
+namespace BookHub
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveWebAssemblyComponents();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveWebAssemblyRenderMode()
+                .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+            app.UseSwagger();
+            object value = app.UseSwaggerUI();
+
+            app.MapControllers(); // BẮT BUỘC
+
+            app.Run();
+        }
+    }
+}
